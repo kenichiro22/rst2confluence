befde7a79997ae510776f72f6a425187a4061bcf
Christian Weiske <christian.weiske@netresearch.de>
2011-11-23 13:16:19 +0100

    setup script, another reorganization :)

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rst2confluence',
+      version='0.1',
+      description='reStructuredText-to-Confluence markup converter',
+      author='Kenichiro TANAKA',
+      author_email='tanaka.kenichiro@gmail.com',
+      url='https://github.com/kenichiro22/rst2confluence',
+      py_modules=['rst2confluence.confluence'],
+      package_dir={'rst2confluence': 'src/rst2confluence'},
+      scripts=['scripts/rst2confluence']
+     )
