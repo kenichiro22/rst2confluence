6755fd49522157f5fb5b80ee43a2f51cff19621f
Christian Weiske <christian.weiske@netresearch.de>
2012-07-26 16:05:43 +0200

    make it possible to pass single test file names to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # Runs rst2confluence.py on all .rst files in test/ and compares them
 # to the expected output (.exp)
-for i in test/*.rst
-do
+if [ $# -eq 0 ]; then
+    files=test/*.rst
+else
+    files="$*"
+fi
+
+for i in $files; do
     echo -n Testing $i
     expFile="$i.exp"
     outFile="$i.out"
@@ -11,12 +16,12 @@ do
 
     diff -u "$expFile" "$outFile" > "$diffFile"
     if [ "$?" -ne "0" ]; then
-        echo " \033[01;31merror\033[00m"
+        echo " \033[00;31merror\033[00m"
         cat "$diffFile" | colordiff
         break;
     else
         #all fine
-        echo " \033[01;32mok\033[00m"
+        echo " \033[00;32mok\033[00m"
         rm "$outFile"
         rm "$diffFile"
     fi
